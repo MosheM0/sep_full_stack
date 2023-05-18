2dc683cad326208b1388ce6a471108ef7940bcb6
Shahar Best <bestshahar9@gmail.com>
2023-05-18 21:13:54 +0300

    add bestSinger endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get('/bestSinger', (req, res) => {
+    res.send('avi biter');
+});
+
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
